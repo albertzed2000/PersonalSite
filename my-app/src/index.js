effac4b897bd3e43d0a3070d3fc650bee161b74d
Albert Zheng <mralbertzheng@gmail.com>
2020-05-22 03:25:55 -0700

    initial personalizations (removed spinning img)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TestComp from "./App";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <TestComp />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
