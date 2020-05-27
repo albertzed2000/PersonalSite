97af4937fa991c086946d6733ea073fa21782758
Albert Zheng <mralbertzheng@gmail.com>
2020-05-27 02:19:35 -0700

    started projects page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Intro from "./parts/2intro/Intro";
 import About from "./parts/3about/About";
+import Projects from "./parts/4projects/Projects"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Intro />
     <About />
+    <Projects />
     
   </React.StrictMode>,
   document.getElementById('root')
---
+++ my-app/src/parts/4projects/Projects.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Projects.css"
+
+
+class Projects extends React.Component{
+
+    render(){
+
+        return(
+
+            <div className="projects-whole">
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                text text
+            </div>
+        )
+    }
+}
+
+export default Projects;
