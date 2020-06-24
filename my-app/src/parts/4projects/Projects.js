aa60ff45b80796094d4ae242baacea29c694a54a
Albert Zheng <albert.zheng@mail.utoronto.ca>
2020-06-24 15:04:32 -0700

    added RazeStats Project

--- my-app/src/parts/4projects/Projects.js
+++ my-app/src/parts/4projects/Projects.js
@@ -24,6 +24,7 @@ class Projects extends React.Component{
         this.adventureClick = this.adventureClick.bind(this);
         this.conversationClick = this.conversationClick.bind(this);
         this.lolStatsClick = this.lolStatsClick.bind(this);
+        this.razeStatsClick = this.razeStatsClick.bind(this);
     }
 
     
@@ -41,6 +42,10 @@ class Projects extends React.Component{
 
     lolStatsClick(){
         window.open('https://github.com/albertzed2000/LoL-Stats');
+    }
+
+    razeStatsClick(){
+        window.open('https://razestats.com');
     }
     render(){
 
@@ -68,12 +73,19 @@ class Projects extends React.Component{
                             <Card.Img variant="top" src={'assets/img/imaqtpie.jpg'} />'
                             
                             <Card.Body>
-                                <Card.Title>LoL Stats</Card.Title>
+                                <Card.Title>LoL Stats/RazeStats</Card.Title>
                                 <Card.Text>
-                                A web app that integrates the Riot Games API in order to display in-depth gameplay statistics.
-                                Built using React, React-Bootstrap, and AWS Lambda + API Gateway for the api proxy server.
+                                Two web apps that integrate the Riot Games API in order to display in-depth gameplay statistics.
+                                Built using React, React-Bootstrap, and AWS Lambda + API Gateway for the API proxy server.
                                 </Card.Text>
-                                <Button variant="primary" onClick={this.lolStatsClick}><FontAwesomeIcon icon={faGithub} /> See it on Github!</Button>
+                                <Row>
+                                    <Col>   
+                                        <Button variant="primary" onClick={this.lolStatsClick}><FontAwesomeIcon icon={faGithub} /> LoLStats on Github!</Button>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="primary" onClick={this.razeStatsClick}><FontAwesomeIcon/>Go To RazeStats!</Button>
+                                    </Col>
+                                </Row>
                             </Card.Body>
                             </Card>
                             </div>
